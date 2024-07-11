d832bee505b27e7ae9646b3e2d2cebc9fb316eed
Orlando Chaparro <orlychaparro@gmail.com>
2024-07-10 21:11:59 -0400

    avance con factura en componentes

--- src/app/components/row-item/row-item.component.ts
+++ src/app/components/row-item/row-item.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../../models/item';
 
 @Component({
-  selector: 'app-row-item',
+  selector: 'tr[row-item]',
   standalone: true,
   imports: [],
   templateUrl: './row-item.component.html'
 })
 export class RowItemComponent {
+
+  @Input() item!: Item;
+  
 
 }
